c61bdf7210201ece95699a6a18bb50389a9d2b6b
nhnajmul17 <nhnajmul17@gmail.com>
2023-05-29 13:36:53 +0600

    user service created and posted in DB

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response, urlencoded } from 'express'
 import cors from 'cors'
+import userRoute from './app/modules/users/users.route'
 
 const app: Application = express()
 
@@ -8,6 +9,9 @@ app.use(cors())
 //parser
 app.use(express.json())
 app.use(urlencoded({ extended: true }))
+
+//application routes
+app.use('/api/v1/users', userRoute)
 
 //Testing
 app.get('/', (req: Request, res: Response) => {
